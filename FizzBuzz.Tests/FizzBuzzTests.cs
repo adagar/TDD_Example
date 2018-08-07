af5034a39f1d1e05dc0998242c6e65d81403c74c
Adalberto 'Nick' Garza <adagar121987@gmail.com>
2018-08-06 20:24:45 -0400

    Went through two more videos

---
+++ FizzBuzz.Tests/FizzBuzzTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using NUnit.Framework;
+using FizzBuzz;
+
+namespace FizzBuzz.Tests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Buzzer_WhenDefault_ReturnsInput(
+            [Values(1, 2, 4, 7, 8, 11, 13, 14)] int input)
+        {
+            //act
+            string output = FizzBuzzer.GetValue(input);
+
+            //assert
+            Assert.AreEqual(input.ToString(), output);
+        }
+        [Test]
+        public void Buzzer_When3_ReturnsFizz(
+            [Values(3, 6, 9, 12)] int input)
+        {
+            //act
+            string output = FizzBuzzer.GetValue(input);
+
+            //assert
+            Assert.AreEqual("Fizz", output);
+        }
+        [Test]
+        public void Buzzer_When5_ReturnsBuzz(
+            [Values(5, 10)] int input)
+        {
+            //act
+            string output = FizzBuzzer.GetValue(input);
+
+            //assert
+            Assert.AreEqual("Buzz", output);
+        }
+        [Test]
+        public void Buzzer_WhenDiv3AndDiv5_ReturnsFizzBuzz(
+            [Values(15)] int input)
+        {
+            string output = FizzBuzzer.GetValue(input);
+
+            Assert.AreEqual("FizzBuzz", output);
+        }
+
+    }
+}
---
+++ FizzBuzz/FizzBuzzer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzer
+    {
+        public static string GetValue(int i)
+        {
+            string output = String.Empty;
+
+            
+            if(i % 3 == 0)
+            {
+                output += "Fizz";
+            }
+            if(i % 5 == 0)
+            {
+                output += "Buzz";
+            }
+            if (string.IsNullOrEmpty(output))
+                output = i.ToString();
+            return output;
+        }
+    }
+}
